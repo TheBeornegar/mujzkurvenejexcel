f250a856b69607d317cc129fae5c60c0e70504c4
TheBeornegar <47784919+TheBeornegar@users.noreply.github.com>
2019-12-05 12:23:26 +0100

    Create Cell.cs

---
+++ Cell.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LidlExcel2
+{
+    public class Cell
+    {
+        public int Row;
+        public int Column;
+        public string Formula;
+
+    }
+}
